da81a5d865da58e4175c4c5d79bdcd4d83f8c1aa
Neo <neotheprogramist@proton.me>
2024-03-02 00:34:39 +0100

    fix build workflow

--- src/components/button-cancel/button-cancel.tsx
+++ src/components/button-cancel/button-cancel.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export const ButtonCancel = component$(() => {
   return (
-    <button class="border-opacity-15 font-sora cursor-pointer rounded-full border-2 border-white bg-black p-4 text-white">
+    <button class="font-sora cursor-pointer rounded-full border-2 border-white border-opacity-15 bg-black p-4 text-white">
       Cancel
     </button>
   );
