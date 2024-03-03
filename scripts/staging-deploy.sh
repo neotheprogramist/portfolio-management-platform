a74c95dfb540384d05b769be63b01d36ec5bb188
Neo <neotheprogramist@proton.me>
2024-03-03 21:13:17 +0100

    enhance scripts

--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -7,11 +7,14 @@ source k8s/patches/staging/.env.local && \
 
 # Delete the existing Kubernetes resources defined in the staging patch
 # The 'true' command ensures that the script continues even if the delete command fails (e.g., if the resources do not exist)
-kubectl delete --wait -k k8s/patches/staging || true && \
+kubectl \
+  --context kubernetes-admin-cluster.visoft.dev@cluster.visoft.dev \
+  delete --wait -k k8s/patches/staging \
+  || true && \
 
 # Build the Docker image using Podman
 # The image is tagged as 'emeth-staging'
-podman build -t emeth-staging:latest . && \
+podman build --platform linux/amd64 -t emeth-staging:latest . && \
 
 # Push the Docker image
 # The image is pushed to the registry specified by the REGISTRY_USER and REGISTRY_PASS environment variables
@@ -19,14 +22,17 @@ podman push --creds $REGISTRY_USER:$REGISTRY_PASS emeth-staging:latest registry.
 
 # Create or update the Docker registry secret
 # This secret is used by Kubernetes to pull the Docker image from the private registry
-kubectl -n emeth-staging create secret docker-registry regcred \
+kubectl --context kubernetes-admin-cluster.visoft.dev@cluster.visoft.dev \
+  -n emeth-staging create secret docker-registry regcred \
   --docker-server registry.visoft.dev \
   --docker-username $REGISTRY_USER \
   --docker-password $REGISTRY_PASS \
   --dry-run=client \
   -o yaml | \
-  kubectl apply -f - && \
+  kubectl --context kubernetes-admin-cluster.visoft.dev@cluster.visoft.dev \
+    apply -f - && \
 
 # Apply Kubernetes manifests
 # This command creates or updates the Kubernetes resources defined in the staging patch
-kubectl apply --wait -k k8s/patches/staging
+kubectl --context kubernetes-admin-cluster.visoft.dev@cluster.visoft.dev \
+  apply --wait -k k8s/patches/staging
