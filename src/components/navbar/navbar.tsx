347bc9d9e7bd61d35f5deeee7a146e3351177a47
Uacias <filgum0326@gmail.com>
2024-03-07 14:01:09 +0100

    fix sigin bug, fmt

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,11 +4,11 @@ import Logo from "/public/images/logo.png?jsx";
 export const Navbar = component$(() => {
   return (
     <>
-      <div class="grid grid-cols-[1fr_2fr_1fr] items-center justify-between py-2 px-6 text-xs text-gray-400 shadow">
+      <div class="grid grid-cols-[1fr_2fr_1fr] items-center justify-between px-6 py-2 text-xs text-gray-400 shadow">
         <div class="h-[20px] w-[94px]">
           <Logo />
         </div>
-        <Slot/>
+        <Slot />
       </div>
     </>
   );
