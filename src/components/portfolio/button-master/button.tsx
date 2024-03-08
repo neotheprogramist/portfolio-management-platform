a5b4d9e29547084a66263864015bcab0760e814e
Anhelina Kruk <krukanhelina06@gmail.com>
2024-03-08 19:03:01 +0100

    Create portfolio page

--- src/components/portfolio/button-master/button.tsx
+++ src/components/portfolio/button-master/button.tsx
@@ -2,21 +2,16 @@ import { component$ } from "@builder.io/qwik";
 import { twMerge } from "tailwind-merge";
 
 export interface ButtonProps {
-  image?: string;
-  text?: string;
-  newClass?: string;
+    image?: string;
+    text?: string;
+    newClass?: string;
 }
 
 export const Button = component$<ButtonProps>((props) => {
-  return (
-    <button
-      class={twMerge(
-        "flex h-[32px] items-center justify-center gap-2 rounded bg-[#E9E9E9] text-xs",
-        props.newClass,
-      )}
-    >
-      {props.image && <img src={props.image} width="16" height="16" />}
-      <span>{props.text}</span>
-    </button>
-  );
+    return (
+        <button class={twMerge("text-xs h-[32px] flex items-center justify-center gap-2 rounded bg-[#E9E9E9]", props.newClass)}>
+            {props.image && <img src={props.image} width="16" height="16" />}
+            <span>{props.text}</span>
+        </button>
+    );
 });
