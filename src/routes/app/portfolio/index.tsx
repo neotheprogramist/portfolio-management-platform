7ea25d1df40dd52450d1c700a56be7f3cb23ea77
furmanp <przemyslaw.furman@yahoo.com>
2024-03-10 21:40:26 +0100

    WIP - code cleanup

--- src/routes/app/portfolio/index.tsx
+++ src/routes/app/portfolio/index.tsx
@@ -247,11 +247,6 @@ function isValidName(name: string): boolean {
   return name.length > 0 ? name.trim().length > 3 : true;
 }
 
-function isValidAddress(address: string): boolean {
-  return address.length > 0
-    ? address.trim() !== "" && isAddress(address)
-    : true;
-}
 
 function populateTokens(
   selectedWallets: WalletTokensBalances[],
