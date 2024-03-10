87b1e9b8ecdbfebb6f3c954f22dc579d4c93f020
Anhelina Kruk <krukanhelina06@gmail.com>
2024-03-10 21:44:56 +0100

    Start wth groups

--- src/routes/app/portfolio/layout.tsx
+++ src/routes/app/portfolio/layout.tsx
@@ -1,11 +1,5 @@
 import { Slot, component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return (
-    <>
-      <div class="px-[20px] bg-[#F4F4F4] text-black grid grid-rows-[64px_auto] ">
-        <Slot />
-      </div>
-    </>
-  );
+  return <Slot />;
 });
