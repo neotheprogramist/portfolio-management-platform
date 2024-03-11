3a9afccc6573e875c1c93e074b344a6fa73043ae
furmanp <przemyslaw.furman@yahoo.com>
2024-03-11 18:52:05 +0100

    - changed approach to display tokens + wallets they come from

--- src/routes/app/wallets/index.tsx
+++ src/routes/app/wallets/index.tsx
@@ -1,5 +1,11 @@
 import { component$, useSignal, useStore } from "@builder.io/qwik";
-import { Form, routeAction$, zod$, z } from "@builder.io/qwik-city";
+import {
+  Form,
+  routeAction$,
+  zod$,
+  z,
+  routeLoader$,
+} from "@builder.io/qwik-city";
 import jwt, { type JwtPayload } from "jsonwebtoken";
 import { publicClient } from "~/abi/abi";
 import { type Wallet } from "~/interface/auth/Wallet";
@@ -10,13 +16,11 @@ import { SelectedWalletDetails } from "~/components/wallets/details";
 import { ObservedWallet } from "~/components/wallets/observed";
 import { type Balance } from "~/interface/balance/Balance";
 import { type WalletTokensBalances } from "~/interface/walletsTokensBalances/walletsTokensBalances";
-import { isAddress } from "viem";
+import { getAddress, isAddress } from "viem";
 // import ImgSearch from "/public/images/svg/search.svg?jsx";
 // import ImgSearch from "../../../../../public/images/svg/search.svg?jsx";
 import ImgArrowDown from "/public/images/arrowDown.svg?jsx";
 import ImgI from "/public/images/svg/i.svg?jsx";
-import { useObservedWallets } from "~/routes/shared";
-export { useObservedWallets } from "~/routes/shared";
 import {
   fetchSubgraphAccountsData,
   fetchSubgraphOneAccount,
@@ -36,7 +40,94 @@ import {
   getResultAddresses,
   getWalletDetails,
 } from "~/interface/wallets/observedWallets";
+import { formatTokenBalance } from "~/utils/formatBalances/formatTokenBalance";
 
+export const useObservedWallets = routeLoader$(async (requestEvent) => {
+  const db = await connectToDB(requestEvent.env);
+
+  const cookie = requestEvent.cookie.get("accessToken");
+  if (!cookie) {
+    throw new Error("No cookie found");
+  }
+  const { userId } = jwt.decode(cookie.value) as JwtPayload;
+
+  const resultAddresses = await getResultAddresses(db, userId);
+  if (!resultAddresses[0]["->observes_wallet"].out.address.length) {
+    return [];
+  }
+  const observedWalletsAddressesQueryResult =
+    resultAddresses[0]["->observes_wallet"].out.address;
+
+  const subgraphURL = requestEvent.env.get("SUBGRAPH_URL");
+  if (!subgraphURL) {
+    throw new Error("Missing SUBGRAPH_URL");
+  }
+
+  const accounts_ = await fetchSubgraphAccountsData(
+    observedWalletsAddressesQueryResult,
+    subgraphURL,
+  );
+
+  const observedWallets: WalletTokensBalances[] = [];
+
+  for (const acc of accounts_) {
+    const nativeBalance = await publicClient.getBalance({
+      address: getAddress(acc.id) as `0x${string}`,
+      blockTag: "safe",
+    });
+    await db.query(
+      `UPDATE wallet SET nativeBalance = '${nativeBalance}' WHERE address = ${getAddress(acc.id)};`,
+    );
+
+    const walletDetails = await getWalletDetails(db, acc.id);
+    if (!walletDetails.at(0)) return [];
+
+    const walletTokensBalances: WalletTokensBalances = {
+      wallet: {
+        id: walletDetails[0].id,
+        name: walletDetails[0].name,
+        chainId: walletDetails[0].chainId,
+        address: getAddress(acc.id),
+        nativeBalance: nativeBalance,
+      },
+      tokens: [],
+    };
+
+    for (const balance of acc.balances) {
+      const balanceToUpdate = await getBalanceToUpdate(
+        db,
+        acc.id,
+        balance.token.id,
+      );
+      const [updatedBalance] = await db.update<Balance>(
+        `${balanceToUpdate[0].id}`,
+        {
+          value: balance.amount.toString(),
+        },
+      );
+
+      const formattedBalance = formatTokenBalance(
+        updatedBalance.value.toString(),
+        parseInt(balance.token.decimals),
+      );
+
+      if (
+        BigInt(updatedBalance.value) !== BigInt(0) &&
+        formattedBalance !== "0.000"
+      ) {
+        walletTokensBalances.tokens.push({
+          id: balance.token.id,
+          name: balance.token.name,
+          symbol: balance.token.symbol,
+          decimals: parseInt(balance.token.decimals),
+          balance: formattedBalance,
+        });
+      }
+    }
+    observedWallets.push(walletTokensBalances);
+  }
+  return observedWallets;
+});
 export const useAddWallet = routeAction$(
   async (data, requestEvent) => {
     const db = await connectToDB(requestEvent.env);
