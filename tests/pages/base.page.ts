3657eb152362dcf1f141a6c96b0637d9f67ef6a4
Marcin Łesek <daav95@gmail.com>
2024-03-12 22:48:51 +0100

    style: fix ts checks

--- tests/pages/base.page.ts
+++ tests/pages/base.page.ts
@@ -1,12 +1,20 @@
 import type { Page } from "@playwright/test";
 
+interface BasePageProps {
+  page: Page;
+  route: string;
+  selectors: { [key: string]: string };
+}
+
 class BasePage {
   readonly page: Page;
   protected route: string;
   selectors: { [key: string]: string };
 
-  constructor(page: Page) {
+  constructor({ page, route, selectors }: BasePageProps) {
     this.page = page;
+    this.route = route;
+    this.selectors = selectors;
   }
 
   async navigate() {
--- tests/pages/login.page.ts
+++ tests/pages/login.page.ts
@@ -1,18 +1,20 @@
 import { expect, type Page } from "@playwright/test";
-import { routes } from "data/routes";
-import { loginFixture } from "fixtures/login.fixture";
+import { routes } from "../data/routes";
+import { loginFixture } from "../fixtures/login.fixture";
 import BasePage from "./base.page";
 
 class LoginPage extends BasePage {
   constructor(page: Page) {
-    super(page);
-    this.route = routes.login;
-    this.selectors = {
-      title: "h1",
-      useMetamaskButton: 'button[data-test="use-metamask-button"]',
-      useWalletConnectButton: 'button[data-test="use-walletconnect-button"]',
-      howToUseWalletButton: 'button[data-test="how-to-use-wallet-button"]',
-    };
+    super({
+      page,
+      route: routes.login,
+      selectors: {
+        title: "h1",
+        useMetamaskButton: 'button[data-test="use-metamask-button"]',
+        useWalletConnectButton: 'button[data-test="use-walletconnect-button"]',
+        howToUseWalletButton: 'button[data-test="how-to-use-wallet-button"]',
+      },
+    });
   }
 
   async verifyTitle() {
