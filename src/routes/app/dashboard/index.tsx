23866acdae15768d5ceeb501449c81f7763309e9
DariaKozlovska <odriy.lovska@gmail.com>
2024-03-14 16:04:55 +0100

    Fixing dashboard

--- src/routes/app/dashboard/index.tsx
+++ src/routes/app/dashboard/index.tsx
@@ -99,12 +99,13 @@ export const useTotalPortfolioValue = routeLoader$(async (requestEvent) => {
 export default component$(() => {
   const totalPortfolioValue = useTotalPortfolioValue();
   return (
-    <div class="grid grid-cols-4 grid-rows-[384px_1fr] gap-6 overflow-auto border-t border-white border-opacity-15 p-6">
+    <div class="grid grid-cols-4 grid-rows-[48%_48%] gap-6 overflow-auto border-t border-white border-opacity-15 p-6">
       <PortfolioValue totalPortfolioValue={totalPortfolioValue.value} />
-      <div class="border-white-opacity-20 bg-glass col-start-3 row-span-1 row-start-1 rounded-3xl p-4">
-        <div class="mb-4 flex items-center justify-between text-white">
+
+      <div class="border-white-opacity-20 bg-glass col-start-3 row-span-1 row-start-1 grid grid-rows-[32px_1fr] gap-[16px] overflow-auto rounded-[16px] p-[24px] shadow">
+        <div class="flex items-center justify-between text-white">
           <h1 class="text-xl font-semibold">Alerts</h1>
-          <button class="border-buttons rounded-[40px] px-[14px] py-[6px] text-xs font-semibold">
+          <button class="border-buttons rounded-[40px] px-[14px] py-[6px] text-xs font-semibold duration-300 ease-in-out hover:scale-110">
             See All
           </button>
         </div>
@@ -124,7 +125,7 @@ export default component$(() => {
       <div class="border-white-opacity-20 bg-glass col-start-4 row-span-1 row-start-1 grid grid-rows-[32px_1fr] gap-[16px] overflow-auto rounded-[16px] p-[24px] shadow">
         <div class="flex items-center justify-between text-white">
           <h1 class="text-xl font-semibold">Actions</h1>
-          <button class="border-buttons rounded-[40px] px-[14px] py-[6px] text-xs font-semibold">
+          <button class="border-buttons rounded-[40px] px-[14px] py-[6px] text-xs font-semibold duration-300 ease-in-out hover:scale-110">
             See All
           </button>
         </div>
@@ -138,7 +139,7 @@ export default component$(() => {
             </div>
             <div class="bg-glass flex h-[28px] items-center gap-1 rounded-lg border border-yellow-400 p-[8px]">
               <ImgWarning />
-              <p class="text-xs text-yellow-400 md:hidden">Warning</p>
+              <p class="text-xs text-yellow-400 lg:hidden">Warning</p>
             </div>
           </div>
           <Action />
@@ -147,72 +148,59 @@ export default component$(() => {
         </div>
       </div>
 
-      <div class="border-white-opacity-20 bg-glass col-start-1 col-end-5 row-span-1 row-start-2 grid grid-rows-[32px_1fr] gap-[8px] overflow-auto rounded-[16px] p-[24px]">
-        <div class="row-span-1 row-start-1 mb-6 flex h-[32px] items-center justify-between">
+      <div class="border-white-opacity-20 bg-glass col-start-1 col-end-5 row-span-1 row-start-2 grid grid-rows-[32px_32px_1fr] gap-[24px] overflow-auto rounded-[16px] p-[24px]">
+        <div class="row-span-1 row-start-1 flex items-center justify-between">
           <h1 class="text-xl font-semibold text-white">Favourite Tokens</h1>
-          <button class="border-buttons rounded-[40px] px-[14px] py-[6px] text-xs font-semibold">
+          <button class="border-buttons rounded-[40px] px-[14px] py-[6px] text-xs font-semibold duration-300 ease-in-out hover:scale-110">
             Go To Portfolio
           </button>
         </div>
-        {/* <div class="">
-          <table class="w-full overflow-auto text-left text-xs">
-            <thead>
-              <tr class="text-white text-opacity-50">
-                <td>TOKEN NAME</td>
-                <td>QUANTITY</td>
-                <td>VALUE</td>
-                <td class="flex items-center justify-center gap-4">
-                  CHANGE
-                  <div class="bg-glass flex gap-[8px] rounded-[8px] border-white-opacity-20 text-white h-[32px] p-[2px] text-xs">
-                    <button class="color-gradient rounded-[6px] px-[8px]">24h</button>
-                    <button class="px-[8px]">3d</button>
-                    <button class="px-[8px]">30d</button>
-                  </div>
-                </td>
-                <td>WALLET</td>
-                <td>NETWORK</td>
-                <td>SUBPORTFOLIO</td>
-                <td></td>
-              </tr>
-            </thead>
-          </table>
-        </div> */}
-        <div class="row-span-1 row-start-3 h-full overflow-auto">
-          <table class="w-full table-auto overflow-hidden text-left text-sm">
-            <thead class="text-xs">
-              <tr class="text-white text-opacity-50">
-                <td>TOKEN NAME</td>
-                <td>QUANTITY</td>
-                <td>VALUE</td>
-                <td class="flex items-center justify-center gap-4">
-                  CHANGE
-                  <div class="bg-glass flex gap-1 rounded-lg border border-white border-opacity-20 p-1 text-white ">
-                    <button class="color-gradient rounded-lg p-2">24h</button>
-                    <button class="p-2">3d</button>
-                    <button class="p-2">30d</button>
-                  </div>
-                </td>
-                <td>WALLET</td>
-                <td>NETWORK</td>
-                <td>SUBPORTFOLIO</td>
-                <td></td>
-              </tr>
-            </thead>
-            <tbody class="overflow-auto">
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-              <TokenRow />
-            </tbody>
-          </table>
+
+        <div class="row-span-1 row-start-2">
+          <div class="grid grid-cols-[17%_8%_11%_20%_14%_11%_12%_3%] items-center gap-[8px] text-left text-xs uppercase text-white text-opacity-50">
+            <div class="">Token name</div>
+            <div class="">Quantity</div>
+            <div class="">Value</div>
+            <div class="flex items-center justify-center gap-[16px]">
+              <h2 class="uppercase text-gray-500">Change</h2>
+              <div class="bg-glass border-white-opacity-20 flex h-[32px] gap-[8px] rounded-lg p-[2px] text-white">
+                <button class="color-gradient rounded-lg px-[8px]">24h</button>
+                <button class="px-[8px]">3d</button>
+                <button class="px-[8px]">30d</button>
+              </div>
+            </div>
+            <div class="">Wallet</div>
+            <div class="">Network</div>
+            <div class="">Subportfolio</div>
+            <div class=""></div>
+          </div>
+        </div>
+
+        <div class="row-span-1 row-start-3 inline-block h-full min-w-full overflow-auto">
+          <div class="overflow-auto">
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+            <TokenRow />
+          </div>
         </div>
       </div>
     </div>
