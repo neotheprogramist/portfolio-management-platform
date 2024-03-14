b14f0e655471b49877d9315087aef3eeacbf7a58
Anhelina Kruk <krukanhelina06@gmail.com>
2024-03-14 01:13:20 +0100

    Grid & components

---
+++ src/components/groups/group.tsx
@@ -0,0 +1,22 @@
+import { Slot, component$ } from "@builder.io/qwik";
+import ArrowDown from "/public/images/svg/portfolio/arrowDown.svg?jsx";
+import EditIcon from "/public/images/svg/portfolio/edit.svg?jsx";
+
+export interface GroupProps {
+    name?: string;  
+}
+
+export const Group = component$<GroupProps>((props) => {
+    return (
+        <>
+        <div class="col-span-full flex h-[50px] pb-[8px] pt-[24px] text-[14px]">
+            <div class="flex items-center gap-[8px] ">
+            <ArrowDown />
+            <h3>{props.name}</h3>
+            <EditIcon />
+            </div>
+        </div>
+        <Slot/>
+        </>
+    );
+})
