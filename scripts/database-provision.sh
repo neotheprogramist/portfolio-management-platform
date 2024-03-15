fcdbe06c1c850e2f0f8d3ce61afe83ce33d815ad
Marcin Łesek <daav95@gmail.com>
2024-03-15 16:39:33 +0100

    fix: check if running locally instead of CI

--- scripts/database-provision.sh
+++ scripts/database-provision.sh
@@ -2,31 +2,34 @@
 
 set -euo pipefail
 
-# Get the directory of the current script.
-SCRIPT_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# Check if we're not running in CI where we need to source environment variables.
+if [ "$CI" = "false" ]; then
+  # Get the directory of the current script.
+  SCRIPT_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Construct the path to the `.env.local` file by navigating up one directory.
-ENV_LOCAL_FILE_PATH="${SCRIPT_DIR_PATH}/../.env.local"
+  # Construct the path to the `.env.local` file by navigating up one directory.
+  ENV_LOCAL_FILE_PATH="${SCRIPT_DIR_PATH}/../.env.local"
 
-# Check if the `.env.local` file exists.
-if [ ! -f "${ENV_LOCAL_FILE_PATH}" ]; then
-    echo "Error: .env.local file does not exist at ${ENV_LOCAL_FILE_PATH} path."
-    exit 1
+  # Check if the `.env.local` file exists.
+  if [ ! -f "${ENV_LOCAL_FILE_PATH}" ]; then
+      echo "Error: .env.local file does not exist at ${ENV_LOCAL_FILE_PATH} path."
+      exit 1
+  fi
+
+  # Source the `.env.local` file in a way that supports non-exported variables.
+  while IFS='=' read -r key value; do
+    # Skip lines that are empty or start with a hash (comments).
+    [[ -z $key || $key =~ ^# ]] && continue
+
+    # Remove potential leading "export " in each line for file consistency.
+    key=${key#export }
+
+    # Use eval to correctly handle values with spaces.
+    eval export "$key='$value'"
+
+  # Do it through all lines in `.env.local` file.
+  done < "${ENV_LOCAL_FILE_PATH}"
 fi
-
-# Source the `.env.local` file in a way that supports non-exported variables.
-while IFS='=' read -r key value; do
-  # Skip lines that are empty or start with a hash (comments).
-  [[ -z $key || $key =~ ^# ]] && continue
-
-  # Remove potential leading "export " in each line for file consistency.
-  key=${key#export }
-
-  # Use eval to correctly handle values with spaces.
-  eval export "$key='$value'"
-
-# Do it through all lines in `.env.local` file.
-done < "${ENV_LOCAL_FILE_PATH}"
 
 # Import fixtures into db
 surreal import \
