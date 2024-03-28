5a437240e79bfa502d282200f1b4e7cedf2d6f2c
root <root@DESKTOP-4C0DSHR>
2024-03-28 18:01:59 +0100

    fmt

--- src/components/message/Message.stories.tsx
+++ src/components/message/Message.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "storybook-framework-qwik";
-import { Message, MessageProps } from "./Message";
+import { Message, type MessageProps } from "./Message";
 
 const meta: Meta<MessageProps> = {
   component: Message,
@@ -11,28 +11,20 @@ export default meta;
 
 export const MessageDefault: Story = {
   render: (props) => (
-    <Message
-      {...props}
-      message='We are trying to send your money...'
-    />
+    <Message {...props} message="We are trying to send your money..." />
   ),
 };
 export const MessageSuccess: Story = {
   render: (props) => (
-    <Message
-      {...props}
-      variant='success'
-      message='Money successfully sent'
-    />
+    <Message {...props} variant="success" message="Money successfully sent" />
   ),
 };
 export const MessageError: Story = {
   render: (props) => (
     <Message
       {...props}
-      variant='error'
-      message='There was an error while sending your money.'
+      variant="error"
+      message="There was an error while sending your money."
     />
   ),
 };
-
--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,38 +1,46 @@
 import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 
-
 export interface MessageProps {
-  variant?: 'success' | 'error' | 'info',
-  message: string,
+  variant?: "success" | "error" | "info";
+  message: string;
 }
 
-export const Message = component$(({variant = 'info', message = ''}: MessageProps) => {
-  const isVisible = useSignal(true);
+export const Message = component$(
+  ({ variant = "info", message = "" }: MessageProps) => {
+    const isVisible = useSignal(true);
 
-  useTask$(() => {
-    const timeId = setTimeout(() => {
-      isVisible.value = false;
-    }, 5000)
-  
-    return () => {
-      clearTimeout(timeId)
-    }
-  })
-
-  if(!isVisible.value) return null;
-
-  return (<>
-    <div class={`fixed bottom-10 left-full p-2 flex items-center  border  rounded min-w-24 max-w-96 animate-messageArrival ${variant === 'success' ? 'bg-[#EDFEEE] text-[#5CB660] border-[#5CB660]' : variant === 'error' ? 'bg-[#FDEDED] text-[#F16360] border-[#F16360]' : 'bg-[#E5F6FD] text-[#1AB1F5] border-[#1AB1F5]'}`}>
-      <div class={`flex items-center shrink-0 justify-center h-8 w-8 border  rounded-full ${variant === 'success' ? 'border-[#5CB660]' : variant === 'error' ? 'border-[#F16360]' : 'border-[#1AB1F5]'}`}>i</div>
-      <div class="ml-2">{message}</div> 
-      <button class="w-8 h-8 flex items-center justify-center"
-      onClick$={() => {
+    useTask$(() => {
+      const timeId = setTimeout(() => {
         isVisible.value = false;
-      }}>x</button>
-    </div> 
-  </>
-      
-  );
-});
+      }, 5000);
+      return () => {
+        clearTimeout(timeId);
+      };
+    });
 
+    if (!isVisible.value) return null;
 
+    return (
+      <>
+        <div
+          class={`fixed bottom-10 left-full flex min-w-24 max-w-96  animate-messageArrival  items-center rounded border p-2 ${variant === "success" ? "border-[#5CB660] bg-[#EDFEEE] text-[#5CB660]" : variant === "error" ? "border-[#F16360] bg-[#FDEDED] text-[#F16360]" : "border-[#1AB1F5] bg-[#E5F6FD] text-[#1AB1F5]"}`}
+        >
+          <div
+            class={`flex h-8 w-8 shrink-0 items-center justify-center rounded-full  border ${variant === "success" ? "border-[#5CB660]" : variant === "error" ? "border-[#F16360]" : "border-[#1AB1F5]"}`}
+          >
+            i
+          </div>
+          <div class="ml-2">{message}</div>
+          <button
+            class="flex h-8 w-8 items-center justify-center"
+            onClick$={() => {
+              isVisible.value = false;
+            }}
+          >
+            x
+          </button>
+        </div>
+      </>
+    );
+  },
+);
