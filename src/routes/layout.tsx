12aa39f101726972fddd8a3a663010f6ac45e2df
Uacias <filgum0326@gmail.com>
2024-04-01 15:43:30 +0200

    add onPost to root layout for stream

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -33,6 +33,31 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
+export const onPost: RequestHandler = async ({request, json}) => {
+  try {
+    const webhook = await request.json();
+    console.log("body", webhook);
+
+    const transfers = webhook["erc20Transfers"];
+    console.log("transfers", transfers);
+
+    for (const transfer of transfers) {
+      const { from, to, tokenSymbol, valueWithDecimals, triggers } = transfer;
+      console.log("========================");
+      console.log("from", from),
+      console.log("to", to),
+      console.log("tokenSymbol", tokenSymbol),
+      console.log("value", valueWithDecimals),
+      console.log("triggers", triggers);
+      console.log("========================")
+    }
+    json(200, { message: "Success" });
+  } catch (error) {
+    console.error(error);
+    json(500, { message: "Internal Server Error - erc20 transfers failed" });
+  }
+}
+
 export default component$(() => {
   const modalStore = useStore<ModalStore>({
     isConnected: undefined,
