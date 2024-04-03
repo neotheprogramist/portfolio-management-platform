efab2364ed9255e4e3a2e6e8847113e872b5aebf
Anhelina Kruk <krukanhelina06@gmail.com>
2024-04-03 21:56:20 +0200

    add gradients

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,10 @@ import { mainnet, sepolia } from "viem/chains";
 export default component$(() => {
   return (
     <>
-      <div class="grid h-full min-w-80 grid-rows-[2fr_0.5fr] items-center">
+    <div class="background-container">
+      {/* <img src="../../public/assets/icons/vector1.svg" alt="" srcset="" class="w-full"/> */}
+    </div>
+      <div class="content-container grid h-full min-w-80 grid-rows-[2fr_0.5fr] items-center gap-10 pt-36">
         <div class="grid justify-items-center items-center gap-10">
           <IconLogo class="w-28 h-6"/>
           <LoginText/>
