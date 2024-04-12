05e69acfe40b40d42eab0dfb533aedf30c696be9
Uacias <filgum0326@gmail.com>
2024-04-12 11:34:46 +0200

    lint fmt

--- src/components/select/select.stories.tsx
+++ src/components/select/select.stories.tsx
@@ -10,9 +10,23 @@ type Story = StoryObj<SelectProps>;
 export default meta;
 
 export const WalletType: Story = {
-  render: () => <Select labelText="Type" text="Executable" value="" onValueChange={() => {}}></Select>,
+  render: () => (
+    <Select
+      labelText="Type"
+      text="Executable"
+      value=""
+      onValueChange={() => {}}
+    ></Select>
+  ),
 };
 
 export const WalletNetwork: Story = {
-  render: () => <Select labelText="Network" text="Ethereum (Default)" value="" onValueChange={() => {}}></Select>,
+  render: () => (
+    <Select
+      labelText="Network"
+      text="Ethereum (Default)"
+      value=""
+      onValueChange={() => {}}
+    ></Select>
+  ),
 };
