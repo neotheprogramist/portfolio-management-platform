2e0bfe335595bb919f94f36a292f3cbc3c952949
Anhelina Kruk <krukanhelina06@gmail.com>
2024-04-16 09:34:03 +0200

    compoentns name 3

--- src/routes/app/dashboard/index.tsx
+++ src/routes/app/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { $, component$, useSignal, useStore, useTask$ } from "@builder.io/qwik";
-import { PortfolioValue } from "~/components/PortfolioValue/PortfolioValue";
+import { PortfolioValue } from "~/components/PortfolioValue/PortfolioValue2";
 import { ActionAlertMessage } from "~/components/ActionAlertsMessage/ActionAlertsMessage";
 import {
   SuccessStatus,
